6748c04329cf65a8f34ee8b03ba0447bff9ea748
fedirjh <fedirjh@gmail.com>
2022-08-27 12:59:59 +0100

    Action Github update  front end

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a79143bc45a08456885de47b20bb2227-2129496009.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
